883f8202e04225502fda96de665977940e848780
Zhenyi He <snowymohehe@gmail.com>
2019-01-15 15:36:53 -0500

    add models; zhu-only is the model from RealSense after removing the background points; viveController is another ply file with same format as zhu-only;

--- UnityProject/Assets/TestLoader.cs
+++ UnityProject/Assets/TestLoader.cs
@@ -7,8 +7,7 @@ public class TestLoader : MonoBehaviour
 {
     public string fileName;
 
-    // Use this for initialization
-    void Start()
+    void sample()
     {
         string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, fileName);
         IntPtr plyIntPtr = PlyLoaderDll.LoadPly(filePath);
@@ -37,6 +36,41 @@ public class TestLoader : MonoBehaviour
             }
             mr.material.mainTexture = www.texture;
         }
+        PlyLoaderDll.UnLoadPly(plyIntPtr);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        // sample();
+
+        TestVertexColor();
+    }
+
+    public Shader shader;
+    public string absFilePath;
+    void TestVertexColor()
+    {
+        IntPtr plyIntPtr = PlyLoaderDll.LoadPly(absFilePath);
+
+        Vector3[] originialVertices = PlyLoaderDll.GetVertices(plyIntPtr);
+        print(originialVertices[0]);
+        Mesh mesh = new Mesh();
+        mesh.vertices = PlyLoaderDll.GetVertices(plyIntPtr);
+        //mesh.uv = PlyLoaderDll.GetUvs(plyIntPtr);
+        //mesh.normals = PlyLoaderDll.GetNormals(plyIntPtr);
+        mesh.colors32 = PlyLoaderDll.GetColors(plyIntPtr);
+        mesh.SetIndices(PlyLoaderDll.GetIndexs(plyIntPtr), MeshTopology.Triangles, 0, true);
+        mesh.name = "mesh";
+        mesh.RecalculateNormals();
+
+        GameObject go = new GameObject();
+        go.name = "meshNew";
+        MeshFilter mf = go.AddComponent<MeshFilter>();
+        mf.mesh = mesh;
+        MeshRenderer mr = go.AddComponent<MeshRenderer>();
+        //mr.material = new Material(Shader.Find("Unlit/VertexColor"));
+        mr.material = new Material(shader);
 
         PlyLoaderDll.UnLoadPly(plyIntPtr);
     }
